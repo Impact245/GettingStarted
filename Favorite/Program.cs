4508a0083dcd82baeac6e72b8d44cf3b59d76d8a
Impact245 <Beasley821@gmail.com>
2017-08-31 10:31:06 -0400

    Real Favorite

---
+++ Favorite/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Favorite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Favorite Food");
+            String h = Console.ReadLine();
+
+            if (h != "Pizza")
+            {
+                Console.WriteLine("You are terribly wrong sir! Your favorite should be pizza.");
+            }
+            else
+            {
+                Console.WriteLine("Look buddy, You're Terrific, Absolutely Stunning!");
+            }
+        }
+    }
+}
