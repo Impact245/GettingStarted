fb44e1b28b2882e9d41de94aa0cf5c7051bb3659
Impact245 <Beasley821@gmail.com>
2017-09-06 11:25:25 -0400

    Password

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordAuthentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String user = "", pass = "";
+            int tries = 0;
+
+            while (tries < 3 && (user != "root" && pass != "letmein"))
+            {
+                Console.WriteLine("Enter Username: ");
+                user = Console.ReadLine();
+
+                Console.WriteLine("Enter Password: ");
+                pass = Console.ReadLine();
+                tries++;
+            }
+
+            if (user == "root" && pass == "letmein")
+            {
+                Console.WriteLine("Congratulations, you pass!!");
+            }
+            else
+            {
+                Console.WriteLine("Mmmmmmmm try again");
+            }
+             
+
+        }
+    }
+}
