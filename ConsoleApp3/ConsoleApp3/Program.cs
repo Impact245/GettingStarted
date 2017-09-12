c74b4e7724ba665f08d361117d4a170c37f9a9db
Impact245 <Beasley821@gmail.com>
2017-09-12 11:19:40 -0400

    Calculator

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Jellyfish;
+
+            Console.WriteLine("Enter an expression: ");
+            Jellyfish = Console.ReadLine();
+
+            String[] Jelly = Jellyfish.Split();
+
+            int campfire;
+            int song;
+            int song2 = 0;
+            String loser;
+
+            campfire = Convert.ToInt32(Jelly[0]);
+            song = Convert.ToInt32(Jelly[2]);
+            loser = Jelly[1];
+
+            if (loser == "+")
+            {
+                Console.WriteLine("Result: " + campfire + " + " + song + " = " + (campfire + song));
+            }
+
+            if (loser == "-")
+            {
+                Console.WriteLine("Result: " + campfire + " - " + song + " = " + (campfire - song));
+            }
+
+            if (loser == "*")
+            {
+                Console.WriteLine("Result: " + campfire + " * " + song + " = " + (campfire * song));
+            }
+
+            if (loser == "/")
+            {
+                Console.WriteLine("Result: " + campfire + " / " + song + " = " + (campfire / song));
+            }
+        }
+    }
+}
