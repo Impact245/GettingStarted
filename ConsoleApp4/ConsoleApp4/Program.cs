0937f0120dff1ab501af4bb4a38d3f7026a3138b
Impact245 <Beasley821@gmail.com>
2017-09-14 11:04:54 -0400

    Palindrome

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String loud;
+
+            Console.WriteLine("Enter a phrase: ");
+            loud = Console.ReadLine();
+
+            char[] b = loud.ToCharArray();
+            b.Reverse();
+
+            String v = new String(b);
+
+            if (loud == v)
+            {
+                Console.WriteLine(loud + " is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine(loud + " is not a palindrome.");
+            }
+
+
+
+
+
+
+        }
+    }
+}
