b214667a0d25b64fa05307d53314fab4549b20e6
Impact245 <Beasley821@gmail.com>
2017-09-18 11:22:35 -0400

    perfnum

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            for (int r = 1; r < 1000000001; r++)
+            {
+                int sum = 0;
+
+                for (int j = 1; j < r; j++)
+                {
+                   if (r % j == 0)
+                    {
+                        sum += j;
+                    }
+
+                }
+
+                if (sum == r)
+                {
+                    Console.WriteLine(r + " is a perfnum.");
+                }
+
+            }
+        }
+    }
+}
